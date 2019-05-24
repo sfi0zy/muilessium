d99f2141578337f9f66c0586b86b812122e54d71
Ivan Bogachev <sfi0zy@gmail.com>
2019-05-24 18:20:09 +0300

    Add events observer to the base component

--- src/js/component.js
+++ src/js/component.js
@@ -14,6 +14,13 @@
 //       Controls (keyboard, mouse or touchscreen) should be initialized here.
 //       It is useful to use utilities from /src/js/utils/ and wrappers from
 //       /src/js/controls/
+//   addEvent(...)
+//       Should be used to add local event to the component. Collect global
+//       events i the global EVENTS object. See /src/js/events.js
+//   addEventListener(...)
+//       Should be used to subscribe to local event of the component.
+//   fireEvent(...)
+//       Executes all callbacks for the local event.
 //
 // -----------------------------------------------------------------------------
 
@@ -25,13 +32,57 @@ export default class Component {
         };
 
         this.state = {};
+
+        this.events = {};
     }
+
 
     initAria() {
         return this;
     }
 
+
     initControls() {
+        return this;
+    }
+
+
+    addEvent(name) {
+        if (!(name in this.events)) {
+            this.events[name] = {
+                callbacks: [],
+                counter: 0
+            };
+        }
+
+        return this;
+    }
+
+
+    addEventListener(name, callback, executeIfAlreadyFired = false) {
+        if ((name in this.events) && (typeof callback === 'function')) {
+            this.events[name].callbacks.push(callback);
+
+            if (executeIfAlreadyFired && (this.events[name].counter > 0)) {
+                callback();
+            }
+        }
+
+        return this;
+    }
+
+
+    fireEvent(name) {
+        if (name in this.events) {
+            this.events[name].counter++;
+
+            this.events[name].callbacks.forEach((callback) => {
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            });
+        }
+
         return this;
     }
 }
