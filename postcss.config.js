43262318d6f9f6bb3f8eed259796a8640eea49f5
Ivan Bogachev <sfi0zy@gmail.com>
2019-06-04 12:25:32 +0300

    Add list-selectors postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,17 @@ module.exports = {
             }
         }),
         require('postcss-fixes')({ preset: 'safe' }),
+        require('list-selectors').plugin((list) => {
+            const inspect = require('util').inspect;
+
+            console.log('\x1b[36m');
+            console.log('SELECTORS:');
+            console.log(inspect(list.selectors, { maxArrayLength: null }));
+            console.log('\x1b[31m');
+            console.log('IDS:');
+            console.log(inspect(list.simpleSelectors.ids, { maxArrayLength: null }));
+            console.log('\x1b[0m');
+        }),
         require('doiuse')(require('./doiuse.config.js')),
         require('cssnano')({
             discardComments: {
